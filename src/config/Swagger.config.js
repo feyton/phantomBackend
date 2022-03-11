7672552b099a3819e7e2dbda6636d1eec349d21f
Fabrice Hafashimana <28519395+feyton@users.noreply.github.com>
2022-03-11 23:19:36 +0200

    chor(setup)- Eslint and prettier (#1)
    - Eslint is used to check for errors
    - Prettier is used to format the code
    - A pre-commit hook has been added to check for errors

--- src/config/Swagger.config.js
+++ src/config/Swagger.config.js
@@ -1,37 +1,37 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 
-dotenv.config();
-const serverUrl = process.env.SERVER_URL || "http://127.0.0.1:3000";
-const serverName = process.env.SERVER_NAME || "LOCAL HOST";
+dotenv.config()
+const serverUrl = process.env.SERVER_URL || 'http://127.0.0.1:3000'
+const serverName = process.env.SERVER_NAME || 'LOCAL HOST'
 
 export const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Phantom backend docs",
-      version: "1.0.0",
-      description: "The Documentation for the phantom project",
-      license: {
-        name: "ISC",
-        url: "https://opensource.org/licenses/ISC",
-      },
-      contact: {
-        name: "Fabrice Hafashimana",
-      },
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Phantom backend docs',
+            version: '1.0.0',
+            description: 'The Documentation for the phantom project',
+            license: {
+                name: 'ISC',
+                url: 'https://opensource.org/licenses/ISC',
+            },
+            contact: {
+                name: 'Fabrice Hafashimana',
+            },
+        },
+        servers: [
+            {
+                url: serverUrl,
+                description: serverName,
+            },
+        ],
     },
-    servers: [
-      {
-        url: serverUrl,
-        description: serverName,
-      },
-    ],
-  },
-  apis: ["src/**/*.js"],
-};
+    apis: ['src/**/*.js'],
+}
 
 export const optionsToCustomizeSwagger = {
-  customCssUrl: "/swagger.css",
-  customSiteTitle: "PHANTOM DOCS",
-  customfavIcon:
-    "https://feyton.github.io/atpl_capstone_fabrice/assets/favicon.png",
-};
+    customCssUrl: '/swagger.css',
+    customSiteTitle: 'PHANTOM DOCS',
+    customfavIcon:
+        'https://feyton.github.io/atpl_capstone_fabrice/assets/favicon.png',
+}
--- src/index/routes.js
+++ src/index/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express'
 
-const router = Router();
+const router = Router()
 
-router.get("*", (req, res) => {
-  return res.status(200).json({ message: "Welcome to the api" });
-});
+router.get('*', (req, res) => {
+    return res.status(200).json({ message: 'Welcome to the api' })
+})
 
 export default router
